1488ec47560e75cf1ecd4831960ae3d505ca5a35
caregor <caregor@gmail.com>
2023-08-03 21:25:54 +0500

    Task 1_1 is done.

---
+++ my_decorator_pack/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.0.1'
+
+__all__ = ['limited_runs_decorator']
+
+from decorator_with_param import limited_runs_decorator
---
+++ my_decorator_pack/decorator_with_param.py
@@ -0,0 +1,17 @@
+"""
+    ---Task 1---
+    Задание No4
+Создайте декоратор с параметром.
+Параметр - целое число, количество запусков декорируемой функции.
+"""
+from typing import Callable
+
+
+def limited_runs_decorator(runs):
+    def decorator(func: Callable):
+        def wrapper(*args, **kwargs):
+            for _ in range(runs):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
